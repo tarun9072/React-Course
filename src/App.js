5cd1f918def9c326551a6808366e82ab89c76cdd
tarun9072 <tarungorayan@gmail.com>
2022-12-27 13:48:45 +0530

    Adding props FIles

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <>
+     <Navbar title="Text Utils" about="About us"/>
+    </>
+  );
+}
+
+export default App;
